dafef69846e4941fce8259c12024168dd8b9905b
Thomas <tseidler@uni-potsdam.de>
2019-10-21 17:49:54 +0200

    Add flyer

---
+++ flyer_Entwurf.cpp
@@ -0,0 +1,22 @@
+class bits_und_basteln{
+
+    int Wann? Mittwochs_um_8;
+    double Wo? Neue_Nachbarschaft_Moabit;
+    bool Was? Bits_und_Basteln!;
+
+    /*
+    Du hast Lust, Dein eigenes Projekt am Computer zu starten?
+    Du willst einfach mal ins Programmieren reinschnuppern?
+    Du hast Lust auf Elektrotechnik?
+    Du magst Pacman?
+
+    Dann komm vorbei,
+        egal ob mit oder ohne Vorkenntnisse,
+        egal ob mit oder ohne eigenen Laptop.
+
+    Wir sind ein Team von IT Connaiseuren, die Lust haben, auch selbst
+    etwas Neues auszuprobieren und Dich nebenbei bei Deinem Einstieg
+    in Bits und Basteln, Programmieren und Technik, zu begleiten und 
+    zu unterstützen.
+   */
+    }
